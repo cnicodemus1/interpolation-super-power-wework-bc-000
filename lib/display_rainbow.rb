91ff8ac96f8b2c599a6ab97a2e6e4e21e0886a08
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:45:30 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-colors= ["Bob", 46, "Juggler", "555-555-5555"]
-stefani= ["Stefani", 49, "Firefighter", "555-111-1111"]
+colors= ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(colors)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
