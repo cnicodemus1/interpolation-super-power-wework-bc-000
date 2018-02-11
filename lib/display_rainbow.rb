e5afce26eb8be7e4120a46253c9828281690492d
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:33:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,8 @@ stefani= ["Stefani", 49, "Firefighter"]
 
 puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
 puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
+
+def print_business_card(person)
+  puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
+  end
+  
