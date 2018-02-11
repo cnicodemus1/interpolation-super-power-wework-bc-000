0bf802b69bfaba75926441f2cdba85d8cdeccc08
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:26:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,3 @@
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+name= "Bob"
 
-colors= "R:red, O:orange, Y:yellow, G:green, B:blue, I:indigo, V:violet"
-
-
-
-def display_board(board)
-  puts colors
-end
+puts "Hello, #{name}"
