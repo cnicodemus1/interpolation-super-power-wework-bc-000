495c231226f5ef6d3709be3b1db000d2cf7d87f9
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:31:14 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
-name= "Bob"
+bob = ["Bob", 46, "Juggler"]
+stefani= ["Stefani", 49, "Firefighter"]
 
-puts "Hello, #{name}"
+puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
+puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
