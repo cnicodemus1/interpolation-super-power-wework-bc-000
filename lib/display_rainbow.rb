ddbc394d1419075da2f82d52d3308f11bc5851fd
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:44:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-bob = ["Bob", 46, "Juggler", "555-555-5555"]
+display_rainbow = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani= ["Stefani", 49, "Firefighter", "555-111-1111"]
 
 def display_rainbow(colors)
@@ -6,5 +6,3 @@ def display_rainbow(colors)
   puts "Contact: #{person[3]}"
   end
   
-  print_business_card(bob)
-  print_business_card(stefani)
