dcf02d22992cad6f05fe21b175d062dc5122699f
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:46:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 colors= ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(colors)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}"
   puts "Contact: #{person[3]}"
   end
   
