b14681717af616dab2ec6c4d3f139863c59ec7c6
Chayanne Nicodemus <cnicodemus@wework.com>
2018-02-11 21:25:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,9 @@
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 
-"R:red, O:orange, Y:yellow, G:green, B:blue, I:indigo, V:violet"
+colors= "R:red, O:orange, Y:yellow, G:green, B:blue, I:indigo, V:violet"
 
 
 
 def display_board(board)
   puts colors
+end
